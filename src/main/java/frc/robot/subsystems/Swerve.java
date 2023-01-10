a3fbd83c7f6a285ae5764775116a29cc7050a42b
David L <lafrancedav15@tcapsstudent.net>
2023-01-10 08:58:32 -0500

    Merge Swerve branch with AprilTags branch

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,25 +1,57 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+import com.kauailabs.navx.frc.AHRS;
+
 import claw.subsystems.SubsystemCLAW;
 
-
+// Extends SubsystemCLAW instead of SubsystemBase
 public class Swerve extends SubsystemCLAW {
+ private static Swerve swerveInstance;
 
-  public Swerve() {}
+	public static Swerve getInstance() {
+		if (swerveInstance == null) swerveInstance = new Swerve();
+		return swerveInstance;
+	}
 
-  public CommandBase exampleMethodCommand() {
+	AHRS gyro;
 
-    return runOnce(
-        () -> {
+	private final Translation2d flPosition, frPosition, rlPosition, rrPosition;
+	public final SwerveDriveKinematics kinematics;
+	public SwerveModuleState[] modules;
+	public ChassisSpeeds speed;
 
-        });
+  public Swerve() {
+	gyro = new AHRS();
+	flPosition = new Translation2d();
+	frPosition = new Translation2d();
+	rlPosition = new Translation2d();
+	rrPosition = new Translation2d();
+	kinematics = new SwerveDriveKinematics(flPosition, frPosition, rlPosition, rrPosition);
+	speed = new ChassisSpeeds();
+	modules = kinematics.toSwerveModuleStates(speed);
   }
 
+  public Rotation2d getAngle (SwerveModuleState swerveModule) {
+	return swerveModule.angle;
+  }
+
+  public void drive(double xInput, double yInput, double theta) {
+	ChassisSpeeds targetSpeeds = new ChassisSpeeds(xInput, yInput, theta);
+	SwerveModuleState[] targetStates = swerveInstance.kinematics.toSwerveModuleStates(targetSpeeds);
+	modules[0] = SwerveModuleState.optimize(targetStates[0], swerveInstance.getAngle(swerveInstance.modules[0]));
+	modules[1] = SwerveModuleState.optimize(targetStates[1], swerveInstance.getAngle(swerveInstance.modules[1]));
+	modules[2] = SwerveModuleState.optimize(targetStates[2], swerveInstance.getAngle(swerveInstance.modules[2]));
+	modules[3] = SwerveModuleState.optimize(targetStates[3], swerveInstance.getAngle(swerveInstance.modules[3]));
+  }
+  public void stop () {
+	speed = new ChassisSpeeds();
+  }
 
   public boolean exampleCondition() {
 
