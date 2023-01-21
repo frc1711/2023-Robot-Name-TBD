e5639c0c331f3b458d1ab3f72752d9d1c563b6e4
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-01-21 16:56:58 -0500

    test custom autobalancer code (not working)

--- src/main/java/frc/robot/commands/auton/Balance.java
+++ src/main/java/frc/robot/commands/auton/Balance.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.auton;
 
 import claw.api.CLAWLogger;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.commands.InputCurve;
@@ -14,13 +13,17 @@ public class Balance extends CommandBase {
     private final Swerve swerve;
     private double startGyroYaw;
     
-    private final PIDController drivePID = new PIDController(0.04, 0, 0.008);
+    private static final InputCurve BASE_SPEED_CURVE = (double x) -> Math.pow(x, 3);
+    private static final InputCurve MOMENTUM_CANCEL_CURVE = (double x) -> Math.pow(x, 0.8);
+    
+    private final AutobalanceFeedforward driveAutobalance = new AutobalanceFeedforward(
+        angle -> InputCurve.apply(BASE_SPEED_CURVE, angle / 8.) * 1.1,
+        speed -> InputCurve.apply(MOMENTUM_CANCEL_CURVE, speed / 60) * 2
+    );
     
     public Balance (Swerve swerve) {
         this.swerve = swerve;
         addRequirements(swerve);
-        
-        drivePID.setTolerance(1);
     }
     
     @Override
@@ -28,13 +31,15 @@ public class Balance extends CommandBase {
         swerve.stop();
         startGyroYaw = swerve.getRobotYaw();
         LOG.sublog("yawInitial").out(startGyroYaw+"");
+        driveAutobalance.reset();
+        driveAutobalance.setTolerance(5);
     }
     
     @Override
     public void execute () {
         LOG.sublog("pitch").out(swerve.getRobotPitch()+"");
         
-        double driveSpeed = -drivePID.calculate(swerve.getRobotPitch(), 0);
+        double driveSpeed = driveAutobalance.calculate(swerve.getRobotPitch());
         LOG.sublog("driveSpeed").out(driveSpeed+"");
         
         LOG.sublog("yaw").out(swerve.getRobotYaw()+"");
