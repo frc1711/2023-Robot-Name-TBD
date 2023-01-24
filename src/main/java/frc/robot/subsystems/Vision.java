c0d6f1d6b09394088deff10c75b5ab92e9a61acb
David L <lafrancedav15@tcapsstudent.net>
2023-01-24 10:12:56 -0500

    Added Limelight

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -8,6 +8,9 @@ import edu.wpi.first.apriltag.AprilTagDetection;
 import edu.wpi.first.apriltag.AprilTagDetector;
 import edu.wpi.first.apriltag.AprilTagPoseEstimator;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision extends SubsystemCLAW{
     
@@ -19,14 +22,45 @@ public class Vision extends SubsystemCLAW{
     }
 
     //TODO: Properly implement Limelight
+    private final NetworkTable limelight;
     private VideoCapture usbCamera;
     private Mat mat;
     private AprilTagDetector detector;
     private AprilTagPoseEstimator estimator;
+    private NetworkTableEntry 
+            tv,
+            tx,
+            ty,
+            ta;
 
     private Vision () {
         usbCamera = new VideoCapture();
         mat = new Mat();
+        limelight = NetworkTableInstance.getDefault().getTable("limelight");
+        tv = limelight.getEntry("tv");
+        tx = limelight.getEntry("tx");
+        ty = limelight.getEntry("ty");
+        ta = limelight.getEntry("ta");
+        limelight.getEntry("ledMode").setDouble(0); //Sets the LED to desired status. 0 to follow current pipeline, 1 for off, 2 for blinking, 3 for on
+        limelight.getEntry("camMode").setDouble(0); //Enables or disables vision processing. 0 for processing, 1 for drive camera
+        limelight.getEntry("pipeline").setDouble(0) //Changes the pipeline of the Limelight
+    }
+
+    
+
+    //Checks if Limelight sees a target, returns false if the value is zero, true if it is 1
+    public boolean seesTarget () {
+        return tv.getDouble(0) == 1;
+    }
+
+    //Returns the horizontal offset of the camera to the target
+    public double getHorizontalOffset () {
+        return tx.getDouble(0);
+    }
+
+    //Returns the vertical offset of the camera to the target
+    public double getVerticalOffset () {
+        return ty.getDouble(0);
     }
 
     //Use Mat images from the USB camera to scan for AprilTags and store their locations
