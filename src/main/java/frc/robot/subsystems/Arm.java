dcf75db8c47b9b160f51934e1085d27c9aecd6cc
David L <lafrancedav15@tcapsstudent.net>
2023-02-11 13:29:08 -0500

    Added clawDebouncer code to operateClaw

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -24,23 +24,26 @@ public class Arm extends SubsystemBase {
     if (armInstance == null) armInstance = new Arm(
                                     new CANSparkMax(IDMap.ARM, MotorType.kBrushless), 
                                     new CANSparkMax(IDMap.CLAW, MotorType.kBrushless), 
-                                    new DigitalInput(IDMap.ARM_LIMIT_SWITCH)
+                                    new DigitalInput(IDMap.ARM_LIMIT_SWITCH),
+                                    new DigitalInput(IDMap.CLAW_LIMIT_SWITCH)
                                     );
     return armInstance;
   }
 
   private CANSparkMax arm, claw;
-  private DigitalInput limitSwitch;
-  private double multiplier;
+  private DigitalInput armLimitSwitch, clawLimitSwitch;
+  private double multiplier = 12;
   private NumericDebouncer clawDebouncer = new NumericDebouncer(new Debouncer(multiplier, DebounceType.kRising));
 
-  public Arm(CANSparkMax arm, CANSparkMax claw, DigitalInput limitSwitch) {
+  public Arm(CANSparkMax arm, CANSparkMax claw, DigitalInput armLimitSwitch, DigitalInput clawLimitSwitch) {
     this.arm = arm;
     this.claw = claw;
+    this.clawLimitSwitch = clawLimitSwitch;
+    this.armLimitSwitch = armLimitSwitch;
   }
 
   public void setArmSpeed(double input) {
-    if (limitSwitch.get()) {
+    if (armLimitSwitch.get()) {
       stop();
     }
     else arm.setVoltage(input * multiplier);
@@ -51,8 +54,13 @@ public class Arm extends SubsystemBase {
   }
 
   public void operateClaw (double input) {
-    if (clawDebouncer.calculate(Optional.of(claw.getOutputCurrent())).get() < 5.0) claw.setVoltage(input * multiplier);
-    else stopClaw();
+    double outputCurrent;
+    if (claw.getOutputCurrent() != 0) {
+      outputCurrent = clawDebouncer.calculate(Optional.of(claw.getOutputCurrent())).get();
+    }
+    else outputCurrent = clawDebouncer.calculate(Optional.empty()).get();
+    if (outputCurrent >= 8) stopClaw();
+    else claw.setVoltage(input);
   }
 
   public void stopClaw() {
@@ -63,6 +71,7 @@ public class Arm extends SubsystemBase {
     stop();
     stopClaw();
   }
+
   @Override
   public void periodic() {
 
