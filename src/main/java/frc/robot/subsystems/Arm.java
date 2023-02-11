b036a6d33464687b96544dfa4532557ea3f9e287
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-02-11 16:36:36 -0500

    end of saturday, working with David on claw

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,9 +4,8 @@
 
 package frc.robot.subsystems;
 
-import java.util.Optional;
-
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.filter.Debouncer;
@@ -14,70 +13,70 @@ import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.IDMap;
-import frc.robot.util.NumericDebouncer;
 
 public class Arm extends SubsystemBase {
-  
-  private static Arm armInstance;
-
-  public static Arm getInstance () {
-    if (armInstance == null) armInstance = new Arm(
-                                    new CANSparkMax(IDMap.ARM, MotorType.kBrushless), 
-                                    new CANSparkMax(IDMap.CLAW, MotorType.kBrushless), 
-                                    new DigitalInput(IDMap.ARM_LIMIT_SWITCH)
-                                    );
-    return armInstance;
-  }
-
-  private CANSparkMax arm, claw;
-  private DigitalInput armLimitSwitch, clawLimitSwitch;
-  private double multiplier = 12;
-  private Debouncer clawDebouncer = new Debouncer(.2, DebounceType.kRising);
-
-  public Arm(CANSparkMax arm, CANSparkMax claw, DigitalInput armLimitSwitch) {
-    this.arm = arm;
-    this.claw = claw;
-    this.armLimitSwitch = armLimitSwitch;
-  }
-
-  public void setArmSpeed(double input) {
-    if (armLimitSwitch.get()) {
-      stop();
+    
+    private static final double HOMING_OUTPUT_CURRENT = 5, CLAW_HOMING_SPEED = 0.1;
+    
+    private static Arm armInstance;
+    
+    public static Arm getInstance () {
+        if (armInstance == null) {
+            armInstance = new Arm(
+                new CANSparkMax(IDMap.ARM, MotorType.kBrushless), 
+                new CANSparkMax(IDMap.CLAW, MotorType.kBrushless), 
+                new DigitalInput(IDMap.ARM_LIMIT_SWITCH)
+            );
+        }
+        return armInstance;
     }
-    else arm.setVoltage(input * multiplier);
-  }
-
-  public void stop () {
-    arm.setVoltage(0);
-  }
-
-  private static final double HOMING_OUTPUT_CURRENT = 5, CLAW_HOMING_SPEED = 0.1;
-
-  public boolean runClawHomingSequence () {
-    if (claw.getOutputCurrent() > HOMING_OUTPUT_CURRENT) {
-      stopClaw();
-      return true;
-    } else {
-      claw.setVoltage(CLAW_HOMING_SPEED);
-      return false;
+    
+    private final CANSparkMax armMotor, clawMotor;
+    private final DigitalInput armLimitSwitch;
+    private final RelativeEncoder clawEncoder;
+    
+    private final Debouncer clawGrabDebouncer = new Debouncer(.2, DebounceType.kRising);
+    
+    public Arm(CANSparkMax armMotor, CANSparkMax clawMotor, DigitalInput armLimitSwitch) {
+        this.armMotor = armMotor;
+        this.clawMotor = clawMotor;
+        this.armLimitSwitch = armLimitSwitch;
+        clawEncoder = clawMotor.getEncoder();
     }
-  }
-
-  public void operateClaw (double input) {
-    claw.setVoltage(input * multiplier);
-  }
-
-  public void stopClaw() {
-    claw.setVoltage(0);
-  }
-
-  public void stopAll() {
-    stop();
-    stopClaw();
-  }
-
-  @Override
-  public void periodic() {
-
-  }
+    
+    public void setArmSpeed(double input) {
+        if (armLimitSwitch.get())
+            stopArm();
+        else
+            arm.setVoltage(input * multiplier);
+    }
+    
+    public void stopArm() {
+        arm.setVoltage(0);
+    }
+    
+    public boolean runClawHomingSequence () {
+        if (claw.getOutputCurrent() > HOMING_OUTPUT_CURRENT) {
+            stopClaw();
+            
+            return true;
+        } else {
+            claw.setVoltage(CLAW_HOMING_SPEED);
+            return false;
+        }
+    }
+    
+    public void stopClaw() {
+        claw.setVoltage(0);
+    }
+    
+    public void stop() {
+        stopArm();
+        stopClaw();
+    }
+    
+    @Override
+    public void periodic() {
+        
+    }
 }
