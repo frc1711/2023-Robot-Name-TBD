a3f6f30636e2124e1b219af2d001672d93bdd36d
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-02-25 17:20:00 -0500

    intake engagement sped up and working well

--- src/main/java/frc/robot/commands/IntakeEngagementCommand.java
+++ src/main/java/frc/robot/commands/IntakeEngagementCommand.java
@@ -1,23 +1,14 @@
 package frc.robot.commands;
 
-import claw.logs.CLAWLogger;
+import claw.math.InputTransform;
 import claw.math.Transform;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
 public class IntakeEngagementCommand extends CommandBase {
     
-    private static final double GRAVITY_FORCE_VOLTAGE = 1.2;
-    
-    private final PIDController pid = new PIDController(0.2, 0, 0);
-    private final SlewRateLimiter limiter = new SlewRateLimiter(15, -15, 0);
-    
     private final Intake intake;
     private final IntakeEngagement engagement;
-    
-    private final CLAWLogger LOG = CLAWLogger.getLogger("intake speed");
     
     public IntakeEngagementCommand (Intake intake, IntakeEngagement engagement) {
         this.intake = intake;
@@ -26,43 +17,40 @@ public class IntakeEngagementCommand extends CommandBase {
     }
     
     private final Transform disengageIntakePositionToVoltage =
-        // Scale from 2.4 at the very bottom to 0.7 at the top
-        ((Transform)((double pos) -> pos * 2.4))
-        .then(Transform.clamp(0.7, 2.4))
+        // Apply a curve so the engagement voltage isn't linear
+        // and it sharply drops off at the beginning
+        InputTransform.THREE_HALVES_CURVE
+        
+        // Scale from 3.8 at the very bottom to 0.5 at the top
+        .then((Transform)((double pos) -> pos * 3.8))
+        .then(Transform.clamp(0.75, 3.8))
         
         // Negate speed because we're disengaging
         .then(Transform.NEGATE);
     
     private final Transform engageIntakePositionToVoltage =
-        // Get a 0 for pos>0.2 and 1 for pos<0.2
-        ((Transform)((double pos) -> 0.2 - pos))
+        // Get a 0 for pos>0.2 and 1 for pos<0.25
+        ((Transform)((double pos) -> 0.25 - pos))
         .then(Transform.SIGN)
         .then(Transform.clamp(0, 1))
         
-        // Multiply by 1.2
-        .then((Transform)((double x) -> 1.2*x));
+        // Multiply for the final voltage
+        .then((Transform)((double x) -> 1.5*x));
     
     @Override
     public void initialize () {
         intake.stop();
-        pid.reset();
-        limiter.reset(0);
     }
     
     @Override
     public void execute () {
         if (engagement == IntakeEngagement.DISENGAGE) {
-            
             // Disengaging
             intake.setEngagementVoltage(disengageIntakePositionToVoltage.apply(intake.getEngagementPosition()));
-            
         } else {
-            
             // Engaging
             intake.setEngagementVoltage(engageIntakePositionToVoltage.apply(intake.getEngagementPosition()));
-            
         }
-        
     }
     
     @Override
