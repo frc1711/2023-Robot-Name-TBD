62e6f204b781a688790c75cdd9788bb121847234
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-02-28 18:19:47 -0500

    configurable arm rotation

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,11 +5,12 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import claw.CLAWRobot;
 import claw.Setting;
-import claw.hardware.AbsoluteEncoderDevice;
+import claw.hardware.Device;
 import claw.math.InputTransform;
 import claw.math.Transform;
 import edu.wpi.first.math.filter.Debouncer;
@@ -17,7 +18,7 @@ import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LiveCommandTester;
@@ -60,15 +61,19 @@ public class Arm extends SubsystemBase {
     
     private final CANSparkMax armMotor, clawMotor;
     
-    private final AbsoluteEncoderDevice<DutyCycleEncoder> armEncoder = new AbsoluteEncoderDevice<>(
+    private final Device<DutyCycle> armEncoder = new Device<>(
         "DIO.ENCODER.ARM.ARM_ENCODER",
-        id -> new DutyCycleEncoder(new DigitalInput(id)),
-        DutyCycleEncoder::close,
-        new Setting<Double>("ARM_ENCODER.CONFIG_OFFSET", () -> 0.),
-        encoder -> Rotation2d.fromDegrees(encoder.getAbsolutePosition())
+        id -> {
+            return new DutyCycle(new DigitalInput(id));
+        },
+        DutyCycle::close
     );
     
     private final Debouncer clawGrabDebouncer = new Debouncer(.8, DebounceType.kRising);
+    
+    private static final Setting<Double> ARM_ENCODER_ZERO = new Setting<>("ARM_ENCODER_CONFIG.ZERO", () -> 0.);
+    private static final Setting<Double> ARM_ENCODER_NINETY = new Setting<>("ARM_ENCODER_CONFIG.NINETY", () -> 1.);
+    
     private double clawEncoderOffset = 0;
     
     private boolean clawHasBeenHomed = false;
@@ -76,7 +81,9 @@ public class Arm extends SubsystemBase {
     
     public Arm(CANSparkMax armMotor, CANSparkMax clawMotor) {
         this.armMotor = armMotor;
+        armMotor.setIdleMode(IdleMode.kBrake);
         this.clawMotor = clawMotor;
+        clawMotor.setIdleMode(IdleMode.kBrake);
         
         XboxController controller = new XboxController(1);
         Transform transform = InputTransform.getInputTransform(
@@ -87,7 +94,8 @@ public class Arm extends SubsystemBase {
         LiveCommandTester tester = new LiveCommandTester(
             "Use controller 1. Y button enables arm control, move the arm using the left joystick. " +
             "Use the A button to enable the claw homing sequence. Use left and right bumpers to " +
-            "release and grab using the claw.",
+            "release and grab using the claw.\n\nHold both triggers and press X to configure the arm down position. " +
+            "Hold both triggers and press B to configure the arm up position.",
             liveValues -> {
                 if (controller.getAButton() && !hasClawBeenHomed()) {
                     
@@ -111,10 +119,18 @@ public class Arm extends SubsystemBase {
                     
                 }
                 
+                if (controller.getLeftTriggerAxis() > 0.8 && controller.getRightTriggerAxis() > 0.8) {
+                    if (controller.getXButton())
+                        ARM_ENCODER_ZERO.set(armEncoder.get().getOutput());
+                    else if (controller.getBButton())
+                        ARM_ENCODER_NINETY.set(armEncoder.get().getOutput());
+                }
+                
                 liveValues.setField("Claw can release", clawCanExtend());
                 liveValues.setField("Claw can grab", clawCanContract());
                 liveValues.setField("Output current", clawMotor.getAppliedOutput());
                 liveValues.setField("Encoder reading", clawMotor.getEncoder().getPosition());
+                liveValues.setField("Arm position", getArmRotation().getDegrees() + " deg");
                 
                 if (controller.getYButton()) {
                     
@@ -136,6 +152,12 @@ public class Arm extends SubsystemBase {
         );
         
         RobotContainer.putConfigSendable("Arm Subsystem", this);
+    }
+    
+    private Rotation2d getArmRotation () {
+        // xProp is the proportion from 0 to 90 degrees
+        double xProp = (armEncoder.get().getOutput() - ARM_ENCODER_ZERO.get()) / (ARM_ENCODER_NINETY.get() - ARM_ENCODER_ZERO.get());
+        return Rotation2d.fromDegrees(xProp * 90);
     }
     
     public void setArmVoltage(double input) {
@@ -208,7 +230,7 @@ public class Arm extends SubsystemBase {
     @Override
     public void initSendable (SendableBuilder builder) {
         builder.addDoubleProperty("Claw output current", clawMotor::getOutputCurrent, null);
-        builder.addDoubleProperty("Arm position", () -> armEncoder.getRotationMeasurement().getDegrees(), null);
+        builder.addDoubleProperty("Arm position", () -> getArmRotation().getDegrees(), null);
     }
     
 }
