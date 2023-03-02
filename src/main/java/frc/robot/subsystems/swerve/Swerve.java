ed8c848b91c67480aac354d526a30554829eb4e7
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-02 10:51:53 -0500

    idk what happened, it keeps switching me to the wrong branch

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -4,20 +4,37 @@
 
 package frc.robot.subsystems.swerve;
 
+import java.util.Optional;
+
 import com.kauailabs.navx.frc.AHRS;
 
 import claw.CLAWRobot;
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.IDMap;
 import frc.robot.LiveCommandTester;
 import frc.robot.RobotContainer;
+import frc.robot.limelight.Limelight;
+import frc.robot.limelight.Limelight.AprilTagData;
 
 public class Swerve extends SubsystemBase {
     
@@ -61,6 +78,35 @@ public class Swerve extends SubsystemBase {
         REAR_LEFT_MODULE_TRANSLATION,
         REAR_RIGHT_MODULE_TRANSLATION
     );
+    
+    private final SwerveDrivePoseEstimator poseEstimator = new SwerveDrivePoseEstimator(
+        kinematics,
+        gyro.getRotation2d(),
+        new SwerveModulePosition[]{
+            flModule.getPosition(),
+            frModule.getPosition(),
+            rlModule.getPosition(),
+            rrModule.getPosition(),
+        },
+        new Pose2d()
+    );
+    
+    private final Constraints robotConstraints = new Constraints(8, 8);
+    
+    private final PIDController
+        autonDriveXController = new PIDController(0, 0, 0),
+        autonDriveYController = new PIDController(0, 0, 0);
+    
+    private final ProfiledPIDController autonDriveThetaController = new ProfiledPIDController(
+        0,
+        0,
+        0,
+        robotConstraints
+    );
+    
+    private final Field2d sendableField = new Field2d();
+    
+    private double measurementOffset = 0;
     
     public Swerve () {
         RobotContainer.putConfigSendable("Swerve Subsystem", this);
@@ -117,10 +163,13 @@ public class Swerve extends SubsystemBase {
     public void moveRobotRelative (ChassisSpeeds speeds) {
         SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
         SwerveModule.desaturateWheelSpeeds(moduleStates);
-        flModule.update(moduleStates[0]);
-        frModule.update(moduleStates[1]);
-        rlModule.update(moduleStates[2]);
-        rrModule.update(moduleStates[3]);
+    }
+    
+    private void setModuleStates (SwerveModuleState[] states) {
+        flModule.update(states[0]);
+        frModule.update(states[1]);
+        rlModule.update(states[2]);
+        rrModule.update(states[3]);
     }
     
     /**
@@ -180,6 +229,41 @@ public class Swerve extends SubsystemBase {
     public void initSendable (SendableBuilder builder) {
         builder.addDoubleProperty("Yaw", this::getRobotYaw, null);
         builder.addDoubleProperty("Pitch", this::getRobotPitch, null);
+    }
+    
+    public Command getControllerCommand (Trajectory trajectory) {
+        return new SwerveControllerCommand(
+            trajectory,
+            poseEstimator::getEstimatedPosition,
+            kinematics,
+            new HolonomicDriveController(
+                autonDriveXController,
+                autonDriveYController,
+                autonDriveThetaController
+            ),
+            this::setModuleStates,
+            this
+        );
+    }
+    
+    @Override
+    public void periodic () {
+        poseEstimator.update(gyro.getRotation2d(), new SwerveModulePosition[]{
+            flModule.getPosition(),
+            frModule.getPosition(),
+            rlModule.getPosition(),
+            rrModule.getPosition(),
+        });
+        
+        Optional<AprilTagData> tag = Limelight.getAprilTag();
+        if (tag.isPresent()) {
+            // TODO: make more robust to bad vision data, figure out why we're not seeing the apriltag in the limelight NT API
+            AprilTagData data = tag.get();
+            double time = Timer.getFPGATimestamp();
+            poseEstimator.addVisionMeasurement(data.robotPose().toPose2d(), time);
+        }
+        
+        sendableField.setRobotPose(poseEstimator.getEstimatedPosition());
     }
     
 }
