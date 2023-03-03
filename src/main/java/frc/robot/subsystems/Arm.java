574c880ac5bfca968576793195350dc693815a4e
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-03 15:54:02 -0500

    digital input encoder class for our use case

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -54,7 +54,7 @@ public class Arm extends SubsystemBase {
         ARM_MIN_ANGLE_DEGREES = -7,
         ARM_MAX_ANGLE_DEGREES = 99;
     
-    private static final double ARM_CURRENT_LIMIT = 25;
+    private static final double ARM_CURRENT_LIMIT = 35;
     
     private static Arm armInstance;
     
@@ -204,11 +204,12 @@ public class Arm extends SubsystemBase {
     public void setArmSpeed (double input) {
         
         double armDegrees = getArmRotation().getDegrees();
-        boolean armCurrentLimitTripped = armCurrentStopSecondDebouncer.calculate(
-            armCurrentStopFirstDebouncer.calculate(
-                armMotor.getOutputCurrent() > ARM_CURRENT_LIMIT
-            )
-        );
+        // boolean armCurrentLimitTripped = armCurrentStopSecondDebouncer.calculate(
+        //     armCurrentStopFirstDebouncer.calculate(
+        //         armMotor.getOutputCurrent() > ARM_CURRENT_LIMIT
+        //     )
+        // );
+        boolean armCurrentLimitTripped = false;
         
         if (!armCurrentLimitTripped) {
             if ((input >= 0 && armDegrees < ARM_MAX_ANGLE_DEGREES) || (input < 0 && armDegrees > ARM_MIN_ANGLE_DEGREES)) {
---
+++ src/main/java/frc/robot/subsystems/DigitalInputEncoder.java
@@ -0,0 +1,127 @@
+package frc.robot.subsystems;
+
+import claw.math.Transform;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DutyCycle;
+
+public class DigitalInputEncoder implements AutoCloseable {
+    
+    private final DutyCycle input;
+    
+    /**
+     * Invert BEFORE applying dutyCycleToDegrees if it has a negative relationship
+     */
+    private final Transform dutyCycleToDegrees;
+    
+    private final boolean hasPositiveRelationship;
+    
+    /**
+     * 
+     * @param source
+     * @param hasPositiveRelationship    Whether or not, when the value of the duty cycle increases, the output angle should increase as a result
+     */
+    public DigitalInputEncoder (DutyCycle input, boolean hasPositiveRelationship, double dutyCycleValue1, AnglePoint p1, AnglePoint p2) {
+        this.input = input;
+        this.hasPositiveRelationship = hasPositiveRelationship;
+        
+        // Inverting the duty cycle so that the transform is fed a positive relationship
+        if (!hasPositiveRelationship) {
+            p1 = new AnglePoint(p1.degrees(), 1 - p1.inputValue());
+            p2 = new AnglePoint(p2.degrees(), 1 - p2.inputValue());
+        }
+        
+        // Change the points so that the second one has higher degrees than the first
+        dutyCycleToDegrees =
+            (p1.degrees() < p2.degrees())
+                ? getTransformFor(p1, p2)
+                : getTransformFor(p2, p1);
+    }
+    
+    public record AnglePoint (double degrees, double inputValue) { }
+    
+    /**
+     * lowPoint has both lower degrees and inputValue than highPoint. If the highPoint has a lower input
+     * value, it's just because it wrapped around from 1 back to 0
+     */
+    private Transform getTransformFor (AnglePoint lowPoint, AnglePoint highPoint) {
+        
+        // Part 1
+        // The goal of this part is to make the relationship from duty cycle input to degrees output linear
+        
+        // This transform will change the provided duty cycle input into one which accounts for wrapping from 1 to 0
+        // (If necessary)
+        Transform adjustDutyCycleInput = i -> i;
+        if (highPoint.inputValue() < lowPoint.inputValue()) {
+            
+            // Must account for wrapping of 1 back to 0:
+            // 0 . . . highPoint       middleInput     lowPoint . . . 1
+            //                    ^^ considered high
+            //                                      ^^ considered low
+            
+            // Get the middle point between the input values
+            double middleInput = (lowPoint.inputValue() + highPoint.inputValue()) / 2;
+            
+            // Give the highPoint a input value of 1 higher to compensate for wrapping when we create the interpolator
+            highPoint = new AnglePoint(highPoint.degrees(), highPoint.inputValue() + 1);
+            
+            // Wrap points below the middleInput to be above 1 to compensate
+            adjustDutyCycleInput = i -> (i < middleInput) ? (i + 1) : (i);
+        }
+        
+        // Part 2
+        // Create the interpolator for the linear relationship
+        
+        Interpolator adjustedDutyCycleToDegrees = new Interpolator(
+            lowPoint.inputValue(), lowPoint.degrees(),
+            highPoint.inputValue(), highPoint.degrees()
+        );
+        
+        return adjustDutyCycleInput.then(adjustedDutyCycleToDegrees::get);
+        
+    }
+    
+    /**
+     * Raw duty cycle from 0 to 1
+     */
+    public double getRawDutyCycleValue () {
+        return input.getOutput();
+    }
+    
+    public double getRotationDegrees () {
+        double input = getRawDutyCycleValue();
+        
+        // Invert input if there's a negative relationship
+        if (!hasPositiveRelationship) input = 1 - input;
+        
+        // Return degrees
+        return dutyCycleToDegrees.apply(input);
+    }
+    
+    public Rotation2d getRotation () {
+        return Rotation2d.fromDegrees(getRotationDegrees());
+    }
+    
+    @Override
+    public void close () {
+        input.close();
+    }
+    
+    private static class Interpolator {
+        
+        private final double x1, xRange, y1, yRange;
+        
+        public Interpolator (double x1, double y1, double x2, double y2) {
+            this.x1 = x1;
+            xRange = x2 - x1;
+            this.y1 = y1;
+            yRange = y2 - y1;
+        }
+        
+        public double get (double x) {
+            double p = (x - x1) / xRange;
+            return y1 + p * yRange;
+        }
+        
+    }
+    
+}
