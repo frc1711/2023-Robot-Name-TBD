56531ef831e5eafddf927c791b0f32fbba1cdfc0
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-03 09:04:32 -0500

    sendable chooser for auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,11 @@ package frc.robot;
 import frc.robot.commands.ArmControlCommand;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.TeleopIntake;
+import frc.robot.commands.auton.BalanceCommandAuton;
+import frc.robot.commands.auton.DriveIntakeAuton;
+import frc.robot.commands.auton.PlaceAndBalanceAuton;
 import frc.robot.commands.auton.PlaceGamePieceTest;
+import frc.robot.commands.auton.PlaceItemAuton;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Conveyor;
@@ -15,13 +19,18 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Arm.ArmPosition;
 import frc.robot.subsystems.swerve.Swerve;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RobotContainer {
+    
+    private final SendableChooser<Supplier<Command>> autonChooser = new SendableChooser<>();
     
     private final XboxController
         driveController = new XboxController(0),
@@ -67,6 +76,8 @@ public class RobotContainer {
         intakeSubsystem.setDefaultCommand(intakeCommand);
         conveyorSubsystem.setDefaultCommand(intakeCommand);
         armSubsystem.setDefaultCommand(armCommand);
+        
+        configAutonChooser();
     }
     
     public static void putConfigSendable (String title, Sendable sendable) {
@@ -78,8 +89,27 @@ public class RobotContainer {
         putConfigSendable(name, command.withName(name).ignoringDisable(true));
     }
     
+    private void configAutonChooser () {
+        autonChooser.addOption("Balance", () -> new BalanceCommandAuton());
+        autonChooser.addOption("Drive Intake", () -> new DriveIntakeAuton());
+        autonChooser.addOption("Place Item", () -> new PlaceItemAuton());
+        autonChooser.addOption("Place and Balance", () -> new PlaceAndBalanceAuton());
+        autonChooser.addOption("TEST", () -> new PlaceGamePieceTest(armSubsystem, clawSubsystem, swerveSubsystem, ArmPosition.MIDDLE));
+    }
+    
+    /**
+     * TODO: PRE-MATCH TO-DOS
+     * 1. Set the ArmPositions so they work
+     * 2. Test auton
+     * 3. Merge with safe-claw-dev branch
+     *    - Test conveyor solo code
+     *    - Test arm current stop code through RCT
+     * 4. 
+     */
+    
     public Command getAutonomousCommand () {
-        return new PlaceGamePieceTest(armSubsystem, clawSubsystem, swerveSubsystem, ArmPosition.MIDDLE);
+        return autonChooser.getSelected().get();
+        // return new PlaceGamePieceTest(armSubsystem, clawSubsystem, swerveSubsystem, ArmPosition.MIDDLE);
         // return swerveSubsystem.getControllerCommand(new Trajectory(Arrays.asList(
         //     new State(0, 0, 1, new Pose2d(), 0),
         //     new State(1, 1, 0, new Pose2d(), 0),
