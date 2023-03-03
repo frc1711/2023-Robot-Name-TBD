93e0853c2b87f596e80fcfa9e3e999045bda9c72
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-03 10:01:36 -0500

    testable drive intake auton

--- src/main/java/frc/robot/commands/auton/DriveIntakeAuton.java
+++ src/main/java/frc/robot/commands/auton/DriveIntakeAuton.java
@@ -1,7 +1,14 @@
 package frc.robot.commands.auton;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.WrapperCommand;
+import frc.robot.subsystems.swerve.Swerve;
 
-public class DriveIntakeAuton extends CommandBase {
+public class DriveIntakeAuton extends WrapperCommand {
+    
+    public DriveIntakeAuton (Swerve swerve) {
+        super(
+            new TimedDriveCommand(swerve, 1, 0, 1, 3)
+        );
+    }
     
 }
