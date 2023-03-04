f667b77c93f56a3a5077484d748600e34caebbcc
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-04 12:47:49 -0500

    fully working balance, slow plus x-mode follow-up

--- src/main/java/frc/robot/commands/auton/BalanceCommandAuton.java
+++ src/main/java/frc/robot/commands/auton/BalanceCommandAuton.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.auton;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.swerve.Swerve;
 
@@ -8,7 +9,8 @@ public class BalanceCommandAuton extends SequentialCommandGroup {
     public BalanceCommandAuton (Swerve swerve) {
         super(
             new DriveStraightCommand(swerve, true, 3),
-            new BalanceSlowCommand(swerve)
+            new BalanceSlowCommand(swerve),
+            new RunCommand(swerve::xMode, swerve).withTimeout(2)
         );
     }
     
