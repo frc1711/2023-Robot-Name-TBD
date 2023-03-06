eb3da62f7714c57c38f7368b8dfb29c7611bf3a7
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-06 17:42:42 -0500

    sped up balance auton so it can go up the charging station

--- src/main/java/frc/robot/commands/auton/DriveStraightCommand.java
+++ src/main/java/frc/robot/commands/auton/DriveStraightCommand.java
@@ -7,9 +7,9 @@ public class DriveStraightCommand extends WrapperCommand {
     
     private static final double DRIVE_SPEED = 2;
     
-    public DriveStraightCommand (Swerve swerve, boolean isForward, double time) {
+    public DriveStraightCommand (Swerve swerve, boolean isForward, double time, double speedMult) {
         super(
-            new TimedDriveCommand(swerve, isForward ? DRIVE_SPEED : -DRIVE_SPEED, 0, 12, time)
+            new TimedDriveCommand(swerve, isForward ? DRIVE_SPEED*speedMult : -DRIVE_SPEED*speedMult, 0, 12, time)
         );
     }
     
