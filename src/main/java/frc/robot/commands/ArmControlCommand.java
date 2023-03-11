d2c77753e28f30cc8b8516f73f8bca640ef065f0
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-03-11 14:36:08 -0500

    Feature/double arm gabriel (#14)
    * working arm
    
    * stabilize arm control ready for testing
    
    * fixed arm stabilization in ArmControlCommand
    
    * arm feedforward accounting for gravity ready for testing
    
    * removed unnecessary feedforward-type system for getSpeedToMoveToRotation
    
    * tuned arm gravity constant so the arm won't slowly fall while driving
    
    * very good arm control
    
    * arm slows near limits
    
    * arm stow button and clears setpoint on command end

--- src/main/java/frc/robot/commands/ArmControlCommand.java
+++ src/main/java/frc/robot/commands/ArmControlCommand.java
@@ -1,10 +1,12 @@
 package frc.robot.commands;
 
+import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import claw.math.InputTransform;
 import claw.math.Transform;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
@@ -16,19 +18,35 @@ public class ArmControlCommand extends CommandBase {
     private final Arm arm;
     private final Claw claw;
     private final DoubleSupplier armControl;
-    private final BooleanSupplier armToLow, armToMid, armToHigh, grabControl, releaseControl;
+    private final BooleanSupplier armToLow, armToMid, armToHigh, armToStow, grabControl, releaseControl;
     
-    private final Transform armSpeedTransform =
+    private Optional<Rotation2d> armSetPosition = Optional.empty();
+    
+    private static Transform getBoundSpeedLimitTransform (DoubleSupplier limitOffsetSupplier, double limitApplicationRange, boolean isPositiveLimit) {
+        Transform limitOffsetToSpeedProp =
+            ((Transform)Math::abs)
+            .then(offset -> offset / limitApplicationRange)
+            .then(Transform.clamp(0, 1));
+        
+        return (speed) -> ((speed > 0) == isPositiveLimit)
+            ? limitOffsetToSpeedProp.apply(limitOffsetSupplier.getAsDouble()) * speed
+            : speed;
+    }
+    
+    private final Transform armControlInputTransform =
         InputTransform.getInputTransform(InputTransform.THREE_HALVES_CURVE, 0.1);
     
+    private final Transform boundSpeedLimitsTransform;
     
     public ArmControlCommand (
         Arm arm,
         Claw claw,
         DoubleSupplier armControl,
+        
         BooleanSupplier armToLow,
         BooleanSupplier armToMid,
         BooleanSupplier armToHigh,
+        BooleanSupplier armToStow,
         
         BooleanSupplier grabControl,
         BooleanSupplier releaseControl
@@ -39,9 +57,15 @@ public class ArmControlCommand extends CommandBase {
         this.armToLow = armToLow;
         this.armToMid = armToMid;
         this.armToHigh = armToHigh;
+        this.armToStow = armToStow;
         
         this.grabControl = grabControl;
         this.releaseControl = releaseControl;
+        
+        boundSpeedLimitsTransform =
+            getBoundSpeedLimitTransform(() -> arm.getArmRotation().getDegrees() - Arm.ARM_MAX_ANGLE_DEGREES, 30, true)
+            .then(getBoundSpeedLimitTransform(() -> arm.getArmRotation().getDegrees() - Arm.ARM_MIN_ANGLE_DEGREES, 40, false));
+        
         addRequirements(arm, claw);
     }
     
@@ -64,34 +88,36 @@ public class ArmControlCommand extends CommandBase {
             claw.operateClaw(ClawMovement.NONE);
         }
         
+        double armControlInput = armControlInputTransform.apply(armControl.getAsDouble());
+        
         // Set arm movement
-        double armInputSpeed = 0;
-        if (armToLow.getAsBoolean() || armToMid.getAsBoolean() || armToHigh.getAsBoolean()) {
-            
-            ArmPosition armSetPosition;
-            if (armToLow.getAsBoolean()) {
-                armSetPosition = ArmPosition.LOW;
-            } else if (armToMid.getAsBoolean()) {
-                armSetPosition = ArmPosition.MIDDLE;
-            } else {
-                armSetPosition = ArmPosition.HIGH;
-            }
-            
-            armInputSpeed = arm.getSpeedToMoveToRotation(armSetPosition.rotation);
-            
-        } else {
-            
-            armInputSpeed = armControl.getAsDouble();
-            
+        if (armControlInput != 0) {
+            armSetPosition = Optional.empty();
+        } else if (armToLow.getAsBoolean()) {
+            armSetPosition = Optional.of(ArmPosition.LOW.rotation);
+        } else if (armToMid.getAsBoolean()) {
+            armSetPosition = Optional.of(ArmPosition.MIDDLE.rotation);
+        } else if (armToHigh.getAsBoolean()) {
+            armSetPosition = Optional.of(ArmPosition.HIGH.rotation);
+        } else if (armToStow.getAsBoolean()) {
+            armSetPosition = Optional.of(ArmPosition.STOWED.rotation);
         }
         
-        arm.setArmSpeed(armSpeedTransform.apply(armInputSpeed));
+        double armInputSpeed = 0;
+        if (armSetPosition.isPresent()) {
+            armInputSpeed = arm.getSpeedToMoveToRotation(armSetPosition.get());
+        } else {
+            armInputSpeed = boundSpeedLimitsTransform.apply(armControlInput);
+        }
+        
+        arm.setArmSpeed(armInputSpeed);
         
     }
     
     @Override
     public void end (boolean interrupted) {
         arm.stop();
+        armSetPosition = Optional.empty();
     }
     
 }
